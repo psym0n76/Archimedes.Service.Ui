0709c2ae6b2125f86974937313f991e05d98de7a
Simon Burton <burt76@outlook.com>
2020-09-03 19:14:56 +0100

    Add project files.

---
+++ Archimedes.Service.Ui/Hubs/IValuesClient.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+
+namespace Archimedes.Service.Ui.Hubs
+{
+    public interface IValuesClient
+    {
+        Task Add(string value);
+        Task Delete(string value);
+    }
+}
---
+++ Archimedes.Service.Ui/Startup.cs
@@ -0,0 +1,66 @@
+using Archimedes.Library.Domain;
+using Archimedes.Service.Ui.Hubs;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Archimedes.Service.Ui
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.Configure<Config>(Configuration.GetSection("AppSettings"));
+            services.AddSignalR();
+            services.AddControllers();
+
+            //todo leave as example
+            //services.AddCors(options =>
+            //{
+            //    options.AddPolicy("AllowAny", x =>
+            //    {
+            //        x.WithOrigins("http://archimedes-ui.com:5103")//, "http://www.archimedes-ui.com:5103","http://localhost:5103")
+            //            .AllowAnyHeader()
+            //            .AllowAnyMethod()
+            //            .AllowCredentials();
+            //    });
+            //});
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            //todo leave as example
+            //app.UseCors("AllowAny");
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+                endpoints.MapHub<ValuesHub>("/Hubs/Values");
+            });
+        }
+    }
+}
