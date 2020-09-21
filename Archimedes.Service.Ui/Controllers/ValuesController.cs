2baf88df3c9232ed578dc7620df5dd343f006112
Simon Burton <burt76@outlook.com>
2020-09-21 21:54:43 +0100

    messing with value controller

--- Archimedes.Service.Ui/Controllers/ValuesController.cs
+++ Archimedes.Service.Ui/Controllers/ValuesController.cs
@@ -6,6 +6,13 @@ using Microsoft.AspNetCore.SignalR;
 
 namespace Archimedes.Service.Ui.Controllers
 {
+
+
+    public class TestData
+    {
+        public string Forecast { get; set; }
+    }
+
     [Produces("application/json")]
     [Route("[controller]")]
     [ApiController]
@@ -19,7 +26,7 @@ namespace Archimedes.Service.Ui.Controllers
             _context = context;
         }
 
-        public static List<string> Source { get; set; } = new List<string>();
+        public static List<TestData> Source { get; set; } = new List<TestData>();
 
         // GET api/values
         [HttpGet]
@@ -27,7 +34,7 @@ namespace Archimedes.Service.Ui.Controllers
         {
             if (Source.Any())
             {
-                return Ok(Source); 
+                return Ok(Source);
             }
 
             return Ok(new List<string>(){});
@@ -36,16 +43,21 @@ namespace Archimedes.Service.Ui.Controllers
 
         // GET api/values/5
         [HttpGet("{id}")]
-        public ActionResult<string> Get(int id)
+        public ActionResult<string> Get(string id)
         {
-            return Source[id];
+            return Source.Find(x => x.Forecast == id).Forecast;
+
+
+
         }
 
         // POST api/values
         [HttpPost]
         public async void Post([FromBody] string value)
         {
-            Source.Add(value);
+
+
+            Source.Add( new TestData(){Forecast = value});
             await _context.Clients.All.SendAsync("Add", value);
         }
 
@@ -53,14 +65,17 @@ namespace Archimedes.Service.Ui.Controllers
         [HttpPut("{id}")]
         public void Put(int id, [FromBody] string value)
         {
-            Source[id] = value;
+            //Source[id] = value;
         }
 
         // DELETE api/values/5
         [HttpDelete("{id}")]
-        public async void Delete(int id)
+        public async void Delete(string id)
         {
-            var item = Source[id];
+            //var item = Source.Remove(id);
+
+            var item = new TestData(){Forecast = id};
+
             Source.Remove(item);
             await _context.Clients.All.SendAsync("Delete", item);
 
