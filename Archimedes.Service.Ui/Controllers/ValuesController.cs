a40ec5d194f5ea7a2a669e952cd800897a79350d
Simon Burton <burt76@outlook.com>
2020-09-24 22:09:20 +0100

    tidy up off siganlR

--- Archimedes.Service.Ui/Controllers/ValuesController.cs
+++ Archimedes.Service.Ui/Controllers/ValuesController.cs
@@ -7,18 +7,11 @@ using Microsoft.AspNetCore.SignalR;
 namespace Archimedes.Service.Ui.Controllers
 {
 
-
-    public class TestData
-    {
-        public string Forecast { get; set; }
-    }
-
     [Produces("application/json")]
     [Route("[controller]")]
     [ApiController]
     public class ValuesController : ControllerBase
     {
-
         private readonly IHubContext<ValuesHub> _context;
 
         public ValuesController(IHubContext<ValuesHub> context)
@@ -26,11 +19,10 @@ namespace Archimedes.Service.Ui.Controllers
             _context = context;
         }
 
-        public static List<TestData> Source { get; set; } = new List<TestData>();
+        public static List<MarketData> Source { get; set; } = new List<MarketData>();
 
-        // GET api/values
         [HttpGet]
-        public ActionResult<IEnumerable<string>> Get()
+        public ActionResult<IEnumerable<MarketData>> Get()
         {
             if (Source.Any())
             {
@@ -38,47 +30,21 @@ namespace Archimedes.Service.Ui.Controllers
             }
 
             return Ok(new List<string>(){});
-            //return Ok( new List<string>(){"No Data"});
         }
 
-        // GET api/values/5
-        [HttpGet("{id}")]
-        public ActionResult<string> Get(string id)
-        {
-            return Source.Find(x => x.Forecast == id).Forecast;
-
-
-
-        }
-
-        // POST api/values
         [HttpPost]
-        public async void Post([FromBody] string value)
+        public async void Post([FromBody] MarketData value)
         {
-
-
-            Source.Add( new TestData(){Forecast = value});
+            Source.Add(value);
             await _context.Clients.All.SendAsync("Add", value);
         }
 
-        // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-            //Source[id] = value;
-        }
-
         // DELETE api/values/5
-        [HttpDelete("{id}")]
-        public async void Delete(string id)
-        {
-            //var item = Source.Remove(id);
-
-            var item = new TestData(){Forecast = id};
-
-            Source.Remove(item);
-            await _context.Clients.All.SendAsync("Delete", item);
-
-        }
+        //[HttpDelete("{id}")]
+        //public async void Delete(string id)
+        //{
+        //    Source.Remove(item);
+        //    await _context.Clients.All.SendAsync("Delete", item);
+        //}
     }
 }
---
+++ Archimedes.Service.Ui/MarketData.cs
@@ -0,0 +1,8 @@
+﻿namespace Archimedes.Service.Ui
+{
+    public class MarketData
+    {
+        public string Name { get; set; }
+        public string Interval { get; set; }
+    }
+}
