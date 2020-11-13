97af87ad4f4aeaf9841933ad3fccdf056a4bfa01
Simon Burton <burt76@outlook.com>
2020-11-13 16:28:56 +0000

    sd

--- Archimedes.Service.Ui/Program.cs
+++ Archimedes.Service.Ui/Program.cs
@@ -45,9 +45,9 @@ namespace Archimedes.Service.Ui
                 .ConfigureServices(services =>
                 {
                     services.AddHostedService<HealthSubscriptionService>();
-                    services.AddHostedService<StrategySubscriptionService>();
-                    services.AddHostedService<MarketSubscriptionService>();
-                    services.AddHostedService <PriceSubscriptionService>();
+                   // services.AddHostedService<StrategySubscriptionService>();
+                   // services.AddHostedService<MarketSubscriptionService>();
+                   // services.AddHostedService<PriceSubscriptionService>();
                 }); // this ensures we have logging
     }
 }
